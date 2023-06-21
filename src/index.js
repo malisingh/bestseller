00ede6498fddce8934025ae4b291a02f4357c776
Malinda Singh <malinda.singh@gmail.com>
2023-06-21 13:30:56 -0400

    added map feature to book list

--- src/index.js
+++ src/index.js
@@ -3,37 +3,40 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+const books = [
+ {
+    author: "Carley Fortune",
+    title: "Every Summer After",
+    img: "./images/book-1.jpg",
+  },
+ {
+    author: "Elin Hilderbrand",
+    title: "The Five-Star Weekend",
+    img: "https://m.media-amazon.com/images/I/71tvrdlSpTL._AC_UL400_.jpg",
+  },
+];
 
-const firstBook = {
-  author: "Carley Fortune",
-  title: "Every Summer After",
-  img: "./images/book-1.jpg",
-};
-
-const secondBook = {
-  author: "Elin Hilderbrand",
-  title: "The Five-Star Weekend",
-  img: "https://m.media-amazon.com/images/I/71tvrdlSpTL._AC_UL400_.jpg",
-};
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
-      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+    <section className='booklist'>
+     {books.map((book) => {
+      const { img, title, author } = book;
+      return <Book img={img} title={title} author={author} />;
+     })}
     </section>
   );
 };
 const Book = (props) => {
+  const { img, title, author} = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
-
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
-}
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
